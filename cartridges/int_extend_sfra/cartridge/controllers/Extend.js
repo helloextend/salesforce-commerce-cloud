b5d53f3a5df6dee26b950a22eb3111e27bf0462c
Khalevskyi <okhalevsky@codifi.me>
2021-08-31 18:14:57 +0300

    EX-26: ADD extend-refund controller

--- cartridges/int_extend_sfra/cartridge/controllers/Extend.js
+++ cartridges/int_extend_sfra/cartridge/controllers/Extend.js
@@ -1,114 +1,13 @@
 'use strict';
 
-/**
- * Controller that adds extra functionality.
- * @module controllers/Extend
- */
-
-/* API Includes */
-var Transaction = require('dw/system/Transaction');
-var URLUtils = require('dw/web/URLUtils');
-
-/* Script Modules */
-var app = require('*/cartridge/scripts/app');
-var guard = require('*/cartridge/scripts/guard');
-
-/* EXTEND */
-var extendHelpers = require('*/cartridge/scripts/extendHelpers');
-
-/**
- * Check productId against the Extend API Offer endpoint
- * This is used in cart to asynchronously enable up-sell modal
- */
-function isEligibleForWarranty() {
-    var BasketMgr = require('dw/order/BasketMgr');
-    var extend = require('*/cartridge/scripts/extend');
-
-    var qs = request.httpParameters;
-    var currentBasket = BasketMgr.getCurrentOrNewBasket();
-    var pid,
-        qty;
-    var response = require('*/cartridge/scripts/util/Response');
-
-    // Query string parameter wasn't provided
-    if (!qs.uuid[0]) {
-        response.renderJSON({
-            isEligible: false
-        });
-        return;
-    }
-
-    // Check if there's already an Extend product attached to this line item or current product is Extend Product
-    for (var i = 0; i < currentBasket.productLineItems.length; i++) {
-        if (currentBasket.productLineItems[i].custom.parentLineItemUUID === qs.uuid[0] || (!empty(currentBasket.productLineItems[i].custom.parentLineItemUUID) && currentBasket.productLineItems[i].getUUID() === qs.uuid[0])) {
-        	response.renderJSON({
-            isEligible: false
-        });
-            return;
-        }
-    }
-
-    for (var i = 0; i < currentBasket.productLineItems.length; i++) {
-        if (currentBasket.productLineItems[i].UUID === qs.uuid[0]) {
-            pid = currentBasket.productLineItems[i].productID;
-            qty = currentBasket.productLineItems[i].quantityValue;
-            break;
-        }
-    }
-
-    // No line item with the provided UUID was found in current basket
-    if (!pid) {
-        response.renderJSON({
-            isEligible: false
-        });
-        return;
-    }
-
-    response.renderJSON({
-    	isEligible: true,
-        pid: pid,
-        qty: qty
-    });
-}
-
-
-/**
- * Handle Extend products when adding to cart from up-sell modal
- */
-function addExtendProduct() {
-    var response = require('*/cartridge/scripts/util/Response');
-    var ProductModel = app.getModel('Product');
-    var cart = app.getModel('Cart').goc();
-    var currentBasket = cart.object;
-    var params = request.httpParameterMap;
-
-    if (!currentBasket) {
-        response.setStatusCode(500);
-        response.renderJSON({
-            error: true,
-            redirectUrl: URLUtils.url('Cart-Show').toString()
-        });
-        return;
-    }
-
-    extendHelpers.createOrUpdateExtendLineItem(cart, params, ProductModel);
-
-    Transaction.wrap(function () {
-        cart.calculate();
-    });
-
-    var updatedBasket = app.getModel('Cart').get();
-
-    response.renderJSON(updatedBasket);
-    return;
-}
+const server = require('server');
 
 /**
  * Function to check whether json valid
  * @param {string} jsonString - json
  * @returns {boolean} - true whether valid, otherwise - false
  */
-function isJsonValid(jsonString) {
+ function isJsonValid(jsonString) {
     try {
         return (JSON.parse(jsonString) && !!jsonString);
     } catch (e) {
@@ -217,13 +116,9 @@ function statusInfo(RESPONSE, contracts, contract, reqContract, refundStatus, me
     contract = {};
 
     RESPONSE.contracts = contracts;
-}
+}​
 
-/**
- * Refund extend warranty from order
- */
-function refund() {
-    var res = require('*/cartridge/scripts/util/Response');
+server.post('Refund', server.middleware.https, function (req, res, next) {
     var Site = require('dw/system/Site');
     var Transaction = require('dw/system/Transaction');
     var OrderMgr = require('dw/order/OrderMgr');
@@ -231,50 +126,50 @@ function refund() {
     var logger = require('dw/system/Logger');
     var extend = require('~/cartridge/scripts/extend');
     var jobHelpers = require('~/cartridge/scripts/jobHelpers');
-​    var headerKey = request.httpHeaders.get('extendsecretkey');
+​    var headerKey = req.httpHeaders.get('extendsecretkey');
     var SECRET_KEY = Site.getCurrent().getCustomPreferenceValue('extendSecretKey');
 
     if (headerKey !== SECRET_KEY) {
-        response.setStatus(400);
-        response.renderJSON({
+        res.setStatusCode(400);
+        res.json({
             error: true,
             message: 'token mismatch'
         });
-        return;
+        return next();
     }
 
     var refundStatus = jobHelpers.refundStatus;
-
-    var data = request.httpParameterMap.requestBodyAsString;
+​
+    var data = req.body;
 
     if (!isJsonValid(data)) {
-        response.setStatus(500);
-        res.renderJSON({
+        res.setStatusCode(500);
+        res.json({
             error: true,
             errorMessage: 'invalid JSON'
         });
-        return;
+        return next();
     } else {
-        data = JSON.parse(request.httpParameterMap.requestBodyAsString);
+        data = JSON.parse(req.body);
     }
 
     var apiOrder = OrderMgr.getOrder(data.orderID);
     if (!apiOrder) {
-        response.setStatus(500);
-        res.renderJSON({
+        res.setStatusCode(500);
+        res.json({
             error: true,
             errorMessage: 'no such order exists'
         });
-        return;
+        return next();
     }
 
     if (!data.products && !data.contracts) {
-        response.setStatus(500);
-        res.renderJSON({
+        res.setStatusCode(500);
+        res.json({
             error: true,
             errorMessage: 'no product or contract array are provided'
         });
-        return;
+        return next();
     }
 
     var RESPONSE = {};
@@ -487,29 +382,11 @@ function refund() {
         }
     }
 
-    res.renderJSON({
+    res.json({
         message: RESPONSE
     });
 
-    return;
-}
-
-
-/*
-* Module exports
-*/
-
-/*
-* Exposed methods.
-*/
-/** Checks if a product is eligible for warranty.
- * @see {@link module:controllers/Extend~isEligibleForWarranty} */
-exports.IsEligibleForWarranty = guard.ensure(['get', 'https'], isEligibleForWarranty);
-
-/** Adds Exend warranty product to cart.
- * @see {@link module:controllers/Extend~addExtendProduct} */
-exports.AddExtendProduct = guard.ensure(['post', 'https'], addExtendProduct);
-
-/** Refund extend warranty from order.
- * @see {@link module:controllers/Extend~refund} */
- exports.Refund = guard.ensure(['post', 'https'], refund);
+    return next();
+});
+​
+module.exports = server.exports();
