1836b6f44d771be97658105c765803d5e42731f1
Nickolay Tkach <ntkach@codifi.me>
2021-12-16 11:31:36 +0200

    EX-168: ADD logging

--- cartridges/int_extend/cartridge/scripts/extend.js
+++ cartridges/int_extend/cartridge/scripts/extend.js
@@ -100,6 +100,11 @@ function getProductsPayload(productBatch) {
 
             var price = product.priceModel.price.available && product.priceModel.price.value > 0 ? Math.round(product.priceModel.price.value * 100) : 0;
 
+            if (!price) {
+                logger.info('Product {0} needed to configure price book', product.getID());
+                continue;
+            }
+
             if (apiVersion !== 'default') {
                 price = {
                     currencyCode: product.priceModel.price.getCurrencyCode(),
@@ -173,7 +178,7 @@ function createRefund(paramObj) {
  * @param {Object} paramObj - object with id of contract and commit type
  * @returns {Object} - response object
  */
- function getOffer(paramObj) {
+function getOffer(paramObj) {
     var endpointName = 'offer';
     var response = webService.makeServiceCall(endpointName, paramObj);
     return response;
